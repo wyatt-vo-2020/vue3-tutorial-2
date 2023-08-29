a0b72c748d292a66d296fa619deb425f6ec0b15c
Wyatt VO <votranquy96@gmail.com>
2023-08-29 11:33:21 +0700

    Try with teleport

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <h1>Welcome to Your Vue.js App</h1>
-  <base-modal
-    title="Title from Component"
-    content="This is content"
-    theme="sales"
-    v-if="isShowModal"
-    @close="onToggleModel"
-  >
-    <template v-slot:header>
-      <h2>This is Title</h2>
-    </template>
-    <label for="">Name</label>
-    <input type="password" />
-    <template v-slot:footer>
-      <h2>This is Footer</h2>
-    </template>
-  </base-modal>
+  <teleport to="body">
+    <base-modal
+      title="Title from Component"
+      content="This is content"
+      theme="sales"
+      v-if="isShowModal"
+      @close="onToggleModel"
+    >
+      <template v-slot:header>
+        <h2>This is Title</h2>
+      </template>
+      <label for="">Name</label>
+      <input type="password" />
+      <template v-slot:footer>
+        <h2>This is Footer</h2>
+      </template>
+    </base-modal>
+  </teleport>
   <button @click="onToggleModel">Toggle Model</button>
 </template>
 
